7096397b6b5e240d8efc95f607236df93f8f0c28
saidivya4654 <saidivya4654@gmail.com>
2021-04-09 18:00:26 +0600

    commit

---
+++ sporty-shoes-1/src/main/java/org/simplilearn/workshop/controller/ProductController.java
@@ -0,0 +1,40 @@
+package org.simplilearn.workshop.controller;
+
+import org.simplilearn.workshop.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.ModelMap;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+@Controller
+@SessionAttributes("name")
+public class ProductController {
+	 @Autowired
+    ProductService service;
+	    @RequestMapping(value="/products", method = RequestMethod.GET)
+	    public String showProductPage(ModelMap model){
+	        return "products";
+	    }
+	 @RequestMapping(value="/products", method = RequestMethod.POST)
+	 public String showProduct(ModelMap model,@RequestParam String date,@RequestParam String category){
+
+	        boolean isValidProduct = service.validateProduct(date,category);
+
+	        if (!isValidProduct) {
+	            model.put("errorMessage", "No Products");
+	            return "welcome";
+	        }
+	        model.put("date", date);
+	        model.put("category", category);
+
+
+	        return "listproducts";
+	    }
+	 
+
+}
+
+
